8b4830c890c4fcf8e6f9196b540a8ffaa99474fd
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2021-11-22 19:53:00 +0300

    Deployment Configuration

---
+++ app/Views/welcome.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Requests</title>
+    <link rel="stylesheet" href="css/Application.css">
+</head>
+
+<body>
+
+    <h1 id="completed-routes">COMPLETED ROUTES</h1>
+    <h3><strong>Properties</strong></h3>
+    <ul>
+        <li><strong>/properties/?</strong>
+            <blockquote>
+                <p>Gets properties belonging to single user</p>
+            </blockquote>
+        </li>
+    </ul>
+    <h3><strong>Listings</strong></h3>
+    <ul>
+        <li><strong>/listings</strong></li>
+    </ul>
+    <blockquote>
+        <p>Gets all listings</p>
+    </blockquote>
+    <ul>
+        <li><strong>/listings/?</strong></li>
+        <blockquote>
+            <p>Gets single listing</p>
+
+        </blockquote>
+
+        <li><strong>/listing/?</strong></li>
+        <blockquote>
+            <p>Gets listings belonging to specific owner</p>
+
+        </blockquote>
+
+</body>
